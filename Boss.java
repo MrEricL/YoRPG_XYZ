2363b2beb3e38e46913e64a37ba20aeb01b53e68
MrEricL <eli6@stuy.edu>
2016-11-25 22:30:50 -0500

    Added boss

---
+++ Boss.java
@@ -0,0 +1,34 @@
+public class Boss extends Character {
+
+    public Boss(){
+	super();
+	about="A super monster!";
+	HP=150*(int)  (Math.random()+1);
+	strength= 150 + (int)(Math.random()* 50);
+	defense= 100+ (int)(Math.random()*5);
+	lives=3;
+	attack=0.8+(int)(Math.random()*1.4);
+    }
+
+    public void specialize(){
+	defense-=50;
+	attack+=3;
+
+    }
+    public void normalize(){
+	defense= 100+ (int)(Math.random()*5);
+      	attack=0.8+(int)(Math.random()*1.4);
+
+
+    }
+    public String about(){
+	String abt;
+	abt="A super monster!!!!";
+	return abt;
+
+
+
+    }
+
+
+}
